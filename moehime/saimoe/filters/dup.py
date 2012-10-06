dbda8af76ab8fa28a4c8a258dcf4382218ae43e4
Wang Xuerui <idontknw.wang@gmail.com>
2012-10-06 13:01:31 +0800

    add duplication filter 增加重复过滤器

--- moehime/saimoe/filters/dup.py
+++ moehime/saimoe/filters/dup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# moehime / saimoe / filter package -- class registry helper
+# moehime / saimoe / filters - simple duplication filter
 #
 # Copyright (C) 2012 Wang Xuerui <idontknw.wang@gmail.com>
 #
@@ -21,8 +21,37 @@
 
 from __future__ import unicode_literals, division
 
-from . import basic
-from . import dup
+from . import filter_manager
+from .base import BaseFilter
+
+_ = lambda x: x
+
+
+@filter_manager.register
+class DuplicationFilter(BaseFilter):
+    '''重复过滤器。
+
+    用来过滤重复 code 及重复识别码的投票。
+
+    '''
+
+    FILTER_NAME = 'dup'
+
+    def examine(self, dataset):
+        self._codes, self._tripcodes = {}, {}
+
+    def _do_judge(self, datum):
+        tripcode, code = datum.author['tripcode'], unicode(datum.code)
+
+        if tripcode in self._tripcodes:
+            return False, _('识别码重复')
+
+        if code in self._codes:
+            return False, _('code 重复')
+
+        self._codes[code] = None
+        self._tripcodes[tripcode] = None
+        return True, None
 
 
 # vim:ai:et:ts=4:sw=4:sts=4:ff=unix:fenc=utf-8:
